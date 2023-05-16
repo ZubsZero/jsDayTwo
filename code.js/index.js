5acf610050643d8405ee454fe525e0c0157cd79f
ZubsZero <zubairmatthee262@gmail.com>
2023-05-16 16:23:02 +0200

    Completed

--- code.js/index.js
+++ code.js/index.js
@@ -1,2 +1,6 @@
-let subjects =  new Array{ "python" , "C++", "VB.net", "Javascript", "Assembly"};
+let subjects = [ "python", "C++", "VB.net", "Javascript", "Assembly"];
+
 console.log(subjects)
+console.log("first subject",subjects[0])
+console.log("Last subject",subjects[4])
+console.log("middle subject",subjects[2])
